0c8e8917c68e5e3716b7019f0cf9f71fa09c9fb4
sagar <sagar@DESKTOP-DS81VAM>
2024-06-09 15:39:56 +0530

    first commit

---
+++ eclipse-workspace/GitAzure/src/pack1/Test1.java
@@ -0,0 +1,16 @@
+package pack1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+		
+		System.out.println("Hello");
+		
+		
+		
+		
+	}
+
+}
